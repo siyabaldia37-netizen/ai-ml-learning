56f1d690f0a3d4046d75e5bc7a720b60c8fa4ee6
Siya <siyabaldia37@gmail.com>
2026-07-02 15:24:02 +0530

    if else condtiton

---
+++ 03_conditional/if-else.py
@@ -0,0 +1,38 @@
+#  IF Conditon Flow
+
+from regex import E, F
+
+
+a = 19
+b = 12
+if(a>b):
+    print(" a is greater than b ")
+
+
+
+#  IF - ELSE CONDITION FLOW 
+
+
+x = 10 
+y = 21
+if(x>y):
+    print("x is greter than y")
+else:
+    print("y is greater than x")
+
+
+   # IF ELSE IF CONDITON FLOW  
+
+e = 10
+f = 23
+g = 30
+if (e>f and e>g ):
+    print("e is gretest ")
+
+elif (f>e and f>g ):
+
+   print(" f is gretest ")
+ 
+else:
+   print("g is the greatest")
+
