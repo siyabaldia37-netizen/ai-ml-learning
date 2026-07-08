bfa243ec96d10ae3244fff19b212ec805d2b012d
Siya <siyabaldia37@gmail.com>
2026-07-08 13:24:49 +0530

    easy problems on datatype

---
+++ 02_Strings/problems_01.py
@@ -0,0 +1,56 @@
+# ESAY PROBLEMS ON STRINGS 
+
+# Create an integer variable named age and assign it your age.
+
+# Create a float variable named height and assign your height.
+
+# Create a string variable named city and store your city name.
+
+# Create a boolean variable named is_student.
+
+# Print all four variables.
+
+# Use type() to print the data type of an integer variable.
+
+# Use type() to print the data type of a float variable.
+
+# Use type() to print the data type of a string variable.
+
+# Use type() to print the data type of a boolean variable.
+
+# Create variables a = 15, b = 2.5, c = "Python", and d = False, then print each variable along with its data type.
+
+
+#  SOLUTIONS
+
+age = 25
+height  =5.5
+city = "narnaul"
+is_student = True
+
+print(age)
+print(height)
+print(city)
+print(is_student)
+
+print(type(age))
+print(type(height))
+print(type(city))
+print(type(is_student))
+
+a = 15
+b = 2.5
+c  ="Python"
+d = False
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
