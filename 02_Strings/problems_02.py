e9a440fba4022b4540038b403484672b69a15c5b
Siya <siyabaldia37@gmail.com>
2026-07-10 18:24:25 +0530

    strings problems

--- 02_Strings/problems_02.py
+++ 02_Strings/problems_02.py
@@ -1,3 +1,61 @@
-print(10+True)
-print(True +5)
-print(False+5)
+# easy strigs problems
+
+# Create a string containing your name and print it.
+
+# Create a string containing your college name.
+
+# Print the first character of "Python".
+
+# Print the last character of "Programming" using negative indexing.
+
+# Print the length of "Artificial Intelligence".
+
+# Convert "python" to uppercase.
+
+# Convert "PYTHON" to lowercase.
+
+# Capitalize the string "hello world".
+
+# Remove leading and trailing spaces from " OpenAI ".
+
+# Replace "Java" with "Python" in "I love Java".
+
+# solutionss
+
+
+name = "siya baldia"
+print(name)
+
+college_name = "PES Universtiy "
+print(college_name)
+
+str = "python"
+print(str[0])
+
+print("Python"[0])
+
+str1 = "programming "
+print(str1[-1])
+
+str3 = "Artificial Intelligence"
+print(len(str3))
+
+str4  = "python"
+print(str4.upper())
+
+str5 = "PYTHON"
+print(str5.lower())
+
+str6 = "hello world"
+print(str6.capitalize())
+
+str7 = " OpenAI"
+print(str7.strip())
+
+str8 = "I love java"
+str9 = str8.replace("java", "python")
+print(str9)
+
+
+
+
