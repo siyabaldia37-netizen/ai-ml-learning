a8d841b2b039219ee2a368bf7500cda9887a30c6
Siya <siyabaldia37@gmail.com>
2026-07-13 23:49:07 +0530

    mini project

---
+++ 01_Basics/mini_project.py
@@ -0,0 +1,13 @@
+print("Smart calculator :")
+
+num1 = int(input("enter your first number:"))
+num2 = int(input("enter your second number:"))
+
+print("addition:" ,num1 + num2)
+print("substraction:" ,num1 - num2)
+print("multiplication:", num1 * num2)
+print("division:", num1 /num2)
+print("floor division:", num1 // num2)
+print('remainder:' ,num1 % num2)
+print("ecponential:" , num1 ** num2)
+
