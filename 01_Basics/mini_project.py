6d1e10ed1203efd733e37aa4b23c252502ecf982
Siya <siyabaldia37@gmail.com>
2026-07-14 00:10:13 +0530

    operators done

--- 01_Basics/mini_project.py
+++ 01_Basics/mini_project.py
@@ -1,13 +1,23 @@
-print("Smart calculator :")
+# print("Smart calculator :")
 
-num1 = int(input("enter your first number:"))
-num2 = int(input("enter your second number:"))
+# num1 = int(input("enter your first number:"))
+# num2 = int(input("enter your second number:"))
 
-print("addition:" ,num1 + num2)
-print("substraction:" ,num1 - num2)
-print("multiplication:", num1 * num2)
-print("division:", num1 /num2)
-print("floor division:", num1 // num2)
-print('remainder:' ,num1 % num2)
-print("ecponential:" , num1 ** num2)
+# print("addition:" ,num1 + num2)
+# print("substraction:" ,num1 - num2)
+# print("multiplication:", num1 * num2)
+# print("division:", num1 /num2)
+# print("floor division:", num1 // num2)
+# print('remainder:' ,num1 % num2)
+# print("ecponential:" , num1 ** num2)
 
+print("=" *35)
+print("total items:")
+print("=" * 35)
+
+item1 = int(input("price one:"))
+item2 = int(input("second one:"))
+total = item1+item2 
+gst =  72
+total_bill = gst + 72
+print(total_bill)
