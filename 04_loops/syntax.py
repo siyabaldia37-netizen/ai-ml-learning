8ff19153e489ae18afa8fa6b0fe9e64ca2dadffd
Siya <siyabaldia37@gmail.com>
2026-07-20 23:09:14 +0530

    loops

---
+++ 04_loops/syntax.py
@@ -0,0 +1,30 @@
+"""i = 1
+while i <= 3 :
+    print('hello')
+    i += 1
+
+print(i)
+
+j = 0
+while j<= 4:
+    print("siyaa")
+    j+=1"""
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i = i - 1
+
+"""j = 0
+for j in range(1, 10, 3):
+    print(j)
+    j+=1
+"""
+"""
+
+for i in range(4):
+    print("Python", i)"""
+
+for i in range(4):
+    print(i)
+print(i)
