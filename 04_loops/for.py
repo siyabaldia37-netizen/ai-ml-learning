69109fb33a663d6c0390482812b5f8cbb1b67395
Siya <siyabaldia37@gmail.com>
2026-07-22 16:44:51 +0530

    while loops

--- 04_loops/for.py
+++ 04_loops/for.py
@@ -1,46 +1,52 @@
-# # problem 1 = print numbers from 1 to 10 
+# # # problem 1 = print numbers from 1 to 10 
+
+# # for i in range(1,11):
+# #     print(i)
+
+# # # problem 2 = print even numbers from 2 to 20
+
+# # for i in range(2, 21, 2):
+# #     print(i)
+
+
+# # # problem 3  = multiplication of 7 
+
+# # for i in range(1,11):
+# #     print(7 * i)
+
+
+# # # problem 4 = print all charatcer from your name 
+
+# # for ch in "SIYA":
+# #     print(ch)
+
+
+
+# # # problem  5=  print numbers decreasign fro 50 to 5 
+
+# # for i in range(50 , 0 , -5):
+# #     print(i)
+
+# # # problrm 6 = print******
+# # for i in range(4):
+# #     print("*", end=" ")
+
+
+
+# for i in range(5 , 101 , 5):
+#     print(i)
+
 
 # for i in range(1,11):
-#     print(i)
-
-# # problem 2 = print even numbers from 2 to 20
-
-# for i in range(2, 21, 2):
-#     print(i)
+#     print(i*i)
 
 
-# # problem 3  = multiplication of 7 
-
-# for i in range(1,11):
-#     print(7 * i)
-
-
-# # problem 4 = print all charatcer from your name 
-
-# for ch in "SIYA":
-#     print(ch)
+# for i in range(5):
+#     print(i+1 , end = " ")
+    
 
 
-
-# # problem  5=  print numbers decreasign fro 50 to 5 
-
-# for i in range(50 , 0 , -5):
-#     print(i)
-
-# # problrm 6 = print******
-# for i in range(4):
-#     print("*", end=" ")
-
-
-
-for i in range(5 , 101 , 5):
+i = 2
+while i<=6:
     print(i)
-
-
-for i in range(1,11):
-    print(i*i)
-
-
-for i in range(5):
-    print(i+1 , end = " ")
-    print(i+1)
+    i = i +2
