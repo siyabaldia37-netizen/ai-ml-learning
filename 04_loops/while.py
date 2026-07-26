83f60ef21c1587c2c21b1b8f1822fec56430a077
Siya <siyabaldia37@gmail.com>
2026-07-26 23:11:14 +0530

    added some problems

--- 04_loops/while.py
+++ 04_loops/while.py
@@ -184,6 +184,45 @@
 
 
 # for i in range(3):
-
 #     for j in range(2):
 #         print(i,j)
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(j+1)
+
+
+# for i in range(3):
+#     for j in range(i+1):
+#         print(j+1, end = " ")
+#         print()
+
+# for i in range(4):
+#     for j in range(i+1):
+#         print(i+1 , end = " ")
+#     print()
+
+
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print(j+1, end  =" ")
+#     print()
+
+# for i in range(4):
+#     for j in range(i+1):
+#         print(4-j , end = " ")
+#     print()
+
+
+# for i in range(4):
+#     for j in range(4-i):
+#         print(i +1 ,end = "")
+#     print()
+
+
+
+for i in range(3):
+    for j in range(3):
+        print(i+1, end  = "")
+    print()
